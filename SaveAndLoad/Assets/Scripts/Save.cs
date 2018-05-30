d24198881fbd93e95f446a96f6ec30fb36405738
WuWeitong <1122212082@qq.com>
2018-05-30 11:58:44 +0800

    JsonXML
    Json XMLGame

---
+++ SaveAndLoad/Assets/Scripts/Save.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  使类可以进行序列化和反序列化
+[System.Serializable]
+public class Save
+{
+
+    public List<int> livingTargetPositions = new List<int>();
+    public List<int> livingMonsterTypes = new List<int>();
+
+    public int shootNum = 0;
+    public int score = 0;
+}
